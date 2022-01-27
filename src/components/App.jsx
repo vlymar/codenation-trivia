c63b755d044ba04d03b09f1968fcacf0171502b4
Victor Lymar <victor@launchdarkly.com>
2022-01-26 16:58:19 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component, useState } from "react";
+import "../css/App.css";
+import data from "../sample_data.json";
+
+function App() {
+  return <div className="app">Trivia!</div>;
+}
+
+export default App;
